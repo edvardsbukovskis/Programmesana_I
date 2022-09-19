e97221029e2b74ee369c729d2ec9a10ab1b01132
edvardsbukovskis <51713537+edvardsbukovskis@users.noreply.github.com>
2022-09-19 09:58:15 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char z;
+    int x, y;
+    int rez;
+
+    cout<<"Ievadi darbibu"<<endl;
+    cin>>z;
+
+    cout<<"Ievadi 2 skaitlus"<<endl;
+    cin>>x>>y;
+    cout<<z<<x<<y;
+
+    rez = x*y;
+
+}
